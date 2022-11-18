73bc6cd0326458005e1fa69a931fa0ac8cb4c454
kassiano <kassiano.e@hotmail.com>
2022-11-18 18:37:13 -0300

    sobre,cores,spa_footer_header

---
+++ assets/js/meu_portfolio.js
@@ -0,0 +1,16 @@
+function carregar_Footer(){
+    $('footer').load('./footer.html');
+}
+function carregar_Nav(){
+    $('header').load('./header.html');
+}
+
+function verifica_Active(){
+    // var path = window.location.path; 
+    // // because the 'href' property of the DOM element is the absolute path
+    // $('.navbar-nav a').each(function() {
+    //   if (this.href === path) {
+    //     $(this).addClass('active');
+    //   }
+    // });
+}

7e62e37190f4c6ceb906583cf8ee4a6954a0f6cf
kassiano <kassiano.e@hotmail.com>
2022-11-27 18:22:02 -0300

    sem_menu

--- assets/js/meu_portfolio.js
+++ assets/js/meu_portfolio.js
@@ -8,31 +8,53 @@ function carregar_projeto_canvas(){
     $('#spa_projetos_canvas').load('./projeto_canvas.html');
 }
 
-function verifica_Active(){
-    $(document).ready(function () {
-
-        // Scroll spy
-        $('body').scrollspy({
-            target: "#nav_scroll"
-        });
-    
-        // Navbar fade
-        changeNavbar();
-    
-        $(window).scroll(function () {
-            changeNavbar();
-        });
-    
-        function changeNavbar() {
-            var navbar = $("#nav_scroll");
-            if ($(this).scrollTop() >= 100) {
-                navbar.addClass("active").removeClass("  ");
-            } else if ($(this).scrollTop() < 100) {
-                navbar.removeClass("active").addClass(" ");
-            }
+function verifica_offcanvas(){
+    var atual_width = document.getElementsByTagName('html')[0].offsetWidth;
+    const offcanvas = document.querySelectorAll('.offcanvas');
+        
+    if (atual_width < 750){
+        for (const offcanva of offcanvas){
+            offcanva.classList.remove('w-50');
+            offcanva.classList.add('w-100');
         }
-    });
+    }else{
+        for (const offcanva of offcanvas){
+            offcanva.classList.remove('w-100');
+            offcanva.classList.add('w-50');
+        }
+    }
 }
+
+function verifica_Active(){
+    // $(document).ready(function () {
+
+    //     // Scroll spy
+    //     $('body').scrollspy({
+    //         target: "#nav_scroll"
+    //     });
+    
+    //     // Navbar fade
+    //     changeNavbar();
+    
+    //     $(window).scroll(function () {
+    //         changeNavbar();
+    //     });
+    
+    //     function changeNavbar() {
+    //         var navbar = $("#nav_scroll");
+    //         if ($(this).scrollTop() >= 100) {
+    //             navbar.addClass("active").removeClass("  ");
+    //         } else if ($(this).scrollTop() < 100) {
+    //             navbar.removeClass("active").addClass(" ");
+    //         }
+    //     }
+    // });
+
+    // var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    //     target: '#nav_scroll'
+    //   })
+}
+
 
 function verifica_Scroll(){
     // $(document).ready(function() {
@@ -53,16 +75,16 @@ function verifica_Scroll(){
     // });
     
     
-    $(window).scroll(function() {
-		var scrollDistance = $(window).scrollTop();
+    // $(window).scroll(function() {
+	// 	var scrollDistance = $(window).scrollTop();
 
-		// Assign active class to nav links while scolling
-		$('.navbar-collapse').each(function(i) {
-            if ($(this).position().top <= scrollDistance) {
-                $('.navigation a.active').removeClass('active');
-                $('.navigation a').eq(i).addClass('active');
-            }
-		});
-    }).scroll();
+	// 	// Assign active class to nav links while scolling
+	// 	$('.navbar-collapse').each(function(i) {
+    //         if ($(this).position().top <= scrollDistance) {
+    //             $('.navigation a.active').removeClass('active');
+    //             $('.navigation a').eq(i).addClass('active');
+    //         }
+	// 	});
+    // }).scroll();
 
 }

b0cc7fb5eaca9e85afbad78d0f3ce38b634bac93
kassiano <kassiano.e@hotmail.com>
2022-11-28 18:54:15 -0300

    fim

--- assets/js/meu_portfolio.js
+++ assets/js/meu_portfolio.js
@@ -25,66 +25,26 @@ function verifica_offcanvas(){
     }
 }
 
-function verifica_Active(){
-    // $(document).ready(function () {
-
-    //     // Scroll spy
-    //     $('body').scrollspy({
-    //         target: "#nav_scroll"
-    //     });
-    
-    //     // Navbar fade
-    //     changeNavbar();
-    
-    //     $(window).scroll(function () {
-    //         changeNavbar();
-    //     });
-    
-    //     function changeNavbar() {
-    //         var navbar = $("#nav_scroll");
-    //         if ($(this).scrollTop() >= 100) {
-    //             navbar.addClass("active").removeClass("  ");
-    //         } else if ($(this).scrollTop() < 100) {
-    //             navbar.removeClass("active").addClass(" ");
-    //         }
-    //     }
-    // });
-
-    // var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    //     target: '#nav_scroll'
-    //   })
-}
-
-
 function verifica_Scroll(){
-    // $(document).ready(function() {
-	// 	$('a[href*=#]').bind('click', function(e) {
-    //         e.preventDefault(); // prevent hard jump, the default behavior
+    window.addEventListener('scroll', ()=>{
+        const sections = document.querySelectorAll('section');
+        const nav_a = document.querySelectorAll('.navbar-nav a');
 
-    //         var target = $(this).attr("href"); // Set the target as variable
-
-    //         // perform animated scrolling by getting top-position of target-element and set it as scroll target
-    //         $('html, body').stop().animate({
-    //                 scrollTop: $(target).offset().top
-    //         }, 600, function() {
-    //                 location.hash = target; //attach the hash (#jumptarget) to the pageurl
-    //         });
-
-    //         return false;
-	// 	});
-    // });
-    
-    
-    // $(window).scroll(function() {
-	// 	var scrollDistance = $(window).scrollTop();
-
-	// 	// Assign active class to nav links while scolling
-	// 	$('.navbar-collapse').each(function(i) {
-    //         if ($(this).position().top <= scrollDistance) {
-    //             $('.navigation a.active').removeClass('active');
-    //             $('.navigation a').eq(i).addClass('active');
-    //         }
-	// 	});
-    // }).scroll();
+        let current = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+            if(pageYOffset >= (sectionTop - sectionHeight / 3)){
+                current = section.getAttribute('id');
+            }
+        });
+        
+        nav_a.forEach(a => {
+            a.classList.remove('active');
+            if(a.classList.contains(current)){
+                a.classList.add('active');
+            }
+        });
+    });
 
 }

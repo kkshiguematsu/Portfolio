ec9d8009ced41c27c909af5855872eb1134e269b
kassiano <kassiano.e@hotmail.com>
2022-12-10 14:20:19 -0300

    armazenamento_de_contatos

---
+++ assets/js/contato.js
@@ -0,0 +1,36 @@
+class Mensagem{
+    constructor(nome,sobrenome,email,telefone,comentario){
+        this.id = Math.floor(Math.random() * 10000);
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.telefone = telefone;
+        this.comentario = comentario;
+    }
+
+    
+}
+
+if (JSON.parse(localStorage.getItem("mensagem"))){
+    mensagens = JSON.parse(localStorage.getItem("mensagem"));
+}else{
+    var mensagens = [];
+}
+
+function salvar_form(){
+    const nome = document.getElementById('form_contato_nome').value;
+    const sobrenome = document.getElementById('form_contato_sobrenome').value;
+    const email = document.getElementById('form_contato_email').value;
+    const telefone = document.getElementById('form_contato_telefone').value;
+    const comentario = document.getElementById('form_contato_comentario').value;
+    
+    const mensagem = new Mensagem(nome,sobrenome,email,telefone,comentario);
+
+    mensagens.push(mensagem);
+
+    localStorage.setItem("mensagem",JSON.stringify(mensagens));
+}
+
+function limpar_lista(){
+    sessionStorage.clear();
+}
